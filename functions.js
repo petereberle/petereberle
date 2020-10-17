9503b27bcf62848d1c32750c7d73424b28bca221
petereberle <eberleapeter@gmail.com>
2020-10-16 21:09:53 -0700

    Update

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
 //FADE OUT ON LINK CLICK
 
-jQuery(document).on("click", "a", function () {
+$(document).on("click", "a", function () {
 
     // get the href attribute
-    var newUrl = jQuery(this).attr("href");
+    var newUrl = $(this).attr("href");
 
     // verify if the new url exists or is a hash
     if ((!newUrl || newUrl[0] === "#") && newUrl != "javascript:void(0);") {
@@ -14,7 +14,7 @@ jQuery(document).on("click", "a", function () {
 
 	if (newUrl != "javascript:void(0);"){
     // now, fadeout the html (whole page)
-    jQuery("html").fadeOut(function () {
+    $("html").fadeOut(function () {
         // when the animation is complete, set the new location
         location = newUrl;
     });
@@ -26,28 +26,17 @@ jQuery(document).on("click", "a", function () {
 
 //ON SCROLL EVENTS
 
-    jQuery(document).ready(function(){
+    $(document).ready(function(){
     
      window.onscroll = function () { resizeHeader(); };
 
-    //var logoImg = document.getElementById("logo_img");
-    //var logoCont = document.getElementById("main_logo");
-    //var barCont = document.getElementById("sticky_bar");
-
      function resizeHeader (){
      if (document.body.scrollTop > 100 | document.documentElement.scrollTop > 100) {
-        jQuery('#logo_img').css({'width' : '100%', 'height' : '100%'});
-        jQuery('#main_logo').addClass('small_logo');
-        jQuery('#sticky_bar').addClass('reduced');
-        jQuery('#menu_inner_container').find('a').addClass('reduced');
-		jQuery('#overlay_wrapper').css({'display' : 'none'});
-		jQuery('#main_menu').addClass('marginReduced');
+		    $('.menu_wrapper').addClass('scroll');
+       TweenLite.to(".menu_wrapper", .2, {background: "#ffffff", boxShadow: "0px 0px 8px 0px rgba(0,0,0,0.2)"});
      } else { 
-         jQuery('#logo_img').css({'width' : '170px', 'height' : '170px'});
-         jQuery('#main_logo').removeClass('small_logo');
-         jQuery('#sticky_bar').removeClass('reduced');
-         jQuery('#menu_inner_container').find('a').removeClass('reduced');
-		 jQuery('#main_menu').removeClass('marginReduced');
+        $('.menu_wrapper').removeClass('scroll');
+        TweenLite.to(".menu_wrapper", .2, {background: "transparent", boxShadow: "0px 0px 0px 0px rgba(0,0,0,0)"});
       }
 
       }
@@ -68,10 +57,10 @@ function toggleNav(x) {x.classList.toggle("bartoggle");}
           if (menu.style.marginLeft < "200%") {
 
 			//HIDE STICKY BAR
-			  jQuery("#sticky_bar").addClass("remove_bar");
+			  $("#sticky_bar").addClass("remove_bar");
 
 			//LOCK BODY SCROLL
-				jQuery("body").addClass("content_locked");
+				$("body").addClass("content_locked");
 
               menu.style.display = "block";
               menu.style.width = "100%";
@@ -82,7 +71,7 @@ function toggleNav(x) {x.classList.toggle("bartoggle");}
           } else {
 
 			//REMOVE BODY SCROLL LOCK
-				jQuery("body").removeClass("content_locked");
+				$("body").removeClass("content_locked");
 
               menu.style.display = "block";
               menu.style.width = "100%";
@@ -91,7 +80,7 @@ function toggleNav(x) {x.classList.toggle("bartoggle");}
 			  navtl.staggerTo("#mobile_menu_cutaway", .5, {opacity: '0', delay:.5}, 0.05);
 
 			//SHOW STICKY BAR
-				jQuery("#sticky_bar").removeClass("remove_bar");
+				$("#sticky_bar").removeClass("remove_bar");
 
               }
           }  
@@ -100,19 +89,19 @@ function toggleNav(x) {x.classList.toggle("bartoggle");}
 
 //CHANGE WA SEARCH PLACEHOLDER
 
-jQuery(document).ready(function(){
+$(document).ready(function(){
 
-	jQuery('#idid_t9YUzjq_searchBox').attr('placeholder', 'Search ArtWalla');
+	$('#idid_t9YUzjq_searchBox').attr('placeholder', 'Search ArtWalla');
 
 });
 
 //REMOVE HOME BUTTON
 
-  jQuery(document).ready( function () {
+  $(document).ready( function () {
 
 function clearFirst () {
 
-	var itemContainer = jQuery(".firstLevel");
+	var itemContainer = $(".firstLevel");
 	//var linkChild = itemContainer.getElementsByTagName('li');
 
 	itemContainer.attr('id', 'first_item');
@@ -121,7 +110,7 @@ function clearFirst () {
 
 document.onload = clearFirst();
 
-	 var firstItem = jQuery("#first_item");
+	 var firstItem = $("#first_item");
 
 	 firstItem.children(":first").attr('id', 'firstMenu');
 
@@ -129,27 +118,27 @@ document.onload = clearFirst();
 
     //QUICKLINK MENU 
 
-    jQuery(document).ready(function()  {
+    $(document).ready(function()  {
     //HOVER REMOVE BORDER
-    jQuery('.quick_link').hover(function() {
-    jQuery(this).siblings().addClass('border_remove');
+    $('.quick_link').hover(function() {
+    $(this).siblings().addClass('border_remove');
       }, function() {
     //LEAVE HOVER ADD BORDER
-      jQuery(this).siblings().removeClass('border_remove');
+      $(this).siblings().removeClass('border_remove');
   });
 
 });
 
   //REVEAL SEARCH BAR
 
-    jQuery(document).ready(function () {
-      jQuery("#search_link").toggle(function(){
+    $(document).ready(function () {
+      $("#search_link").toggle(function(){
 
-        jQuery("#search_bar_container").addClass('show_search');
+        $("#search_bar_container").addClass('show_search');
 
       }, function(){
 
-        jQuery("#search_bar_container").removeClass('show_search');
+        $("#search_bar_container").removeClass('show_search');
 
       }
       );
@@ -168,7 +157,7 @@ document.querySelectorAll('.menu_search').forEach(item => {
               searchContainer.style.width = "100%";
               searchtl.staggerTo("#search_popup_container", .5, {marginRight: '0%', opacity: '1', delay:.5}, 0.05);
 
-              jQuery("body").addClass('search_locked');
+              $("body").addClass('search_locked');
           }
 
       })
@@ -181,7 +170,7 @@ document.querySelectorAll('.menu_search').forEach(item => {
 
     searchtl.staggerTo("#search_popup_container", .5, {marginRight: '-100%', opacity: '0', delay:.5}, 0.05);
 
-    jQuery("body").removeClass('search_locked');
+    $("body").removeClass('search_locked');
    
     }
   }
@@ -190,9 +179,9 @@ document.querySelectorAll('.menu_search').forEach(item => {
 
   function removeProp() {
 
-    jQuery("#senderName").attr('placeholder','');
+    $("#senderName").attr('placeholder','');
 
-    jQuery("#senderAddress").attr('placeholder','');
+    $("#senderAddress").attr('placeholder','');
 
   }
 
@@ -209,7 +198,7 @@ document.querySelectorAll('.menu_search').forEach(item => {
   
   }else{
 
-  jQuery('#error_message').css({'display' : 'none'});
+  $('#error_message').css({'display' : 'none'});
   document.getElementById("senderName").value = '';
   document.getElementById("senderAddress").value = '';
 
@@ -230,9 +219,9 @@ document.querySelectorAll('.menu_search').forEach(item => {
 
   function removeProp() {
 
-    jQuery("#senderNamePage").attr('placeholder','');
+    $("#senderNamePage").attr('placeholder','');
 
-    jQuery("#senderAddresPage").attr('placeholder','');
+    $("#senderAddresPage").attr('placeholder','');
 
   }
 
@@ -249,7 +238,7 @@ document.querySelectorAll('.menu_search').forEach(item => {
   
   }else{
 
-  jQuery('#error_message_page').css({'display' : 'none'});
+  $('#error_message_page').css({'display' : 'none'});
   document.getElementById("senderNamePage").value = '';
   document.getElementById("senderAddressPage").value = '';
 
