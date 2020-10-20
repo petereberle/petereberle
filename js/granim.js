a6cb2895141e7d0f44762953b8bd9d6ae76a0d55
petereberle <eberleapeter@gmail.com>
2020-10-19 23:19:48 -0700

    Bg change

--- js/granim.js
+++ js/granim.js
@@ -1,15 +1,15 @@
 var granimInstance = new Granim({
     element: '#home',
-    direction: 'left-right',
+    direction: 'diagonal',
     isPausedWhenNotInView: true,
     states : {
         "default-state": {
             gradients: [
-                ['#FBAD18', '#EA9B1C'],
-                ['#FBAD18', '#f46626'],
+                ['#FBAD18', '#f4642a'],
+                ['#88c1d3', '#f46626'],
                 ['#EA9B1C', '#FBAD18']
             ],
-            transitionSpeed: 4000
+            transitionSpeed: 2000
         }
     }
 });
--- js/menu_scroll.js
+++ js/menu_scroll.js
@@ -1,5 +1,5 @@
 var scrolltoSection = function scrolltoSection(element) {
   $(window).scrollTo(document.querySelector(element), 1500, {
-    easing: 'easeInOutQuint'
+    easing: 'easeInOutQuart'
   });
 };
