63468853a681288ae23f77e5bbfdd40760259174
petereberle <eberleapeter@gmail.com>
2020-10-22 15:28:09 -0700

    404

--- js/lines.js
+++ js/lines.js
@@ -1,5 +1,8 @@
+var parentWidth = document.querySelector(".heading_block").offsetWidth;
+var parentHeight = document.querySelector(".heading_block").offsetHeight;
+
 function setup() {
-	var canvas = createCanvas(windowWidth, windowHeight);
+	var canvas = createCanvas(parentWidth, parentHeight);
 	canvas.parent('sketch-div');
 	frameRate(5);
 }
@@ -8,9 +11,9 @@ function draw() {
 
 	stroke(255, 255, 255, 20);
 
-	let r = random(windowWidth);
-	let i = random(windowHeight);
-	let g = random(windowWidth);
-	let p = random(windowHeight);
+	let r = random(parentWidth);
+	let i = random(parentHeight);
+	let g = random(parentWidth);
+	let p = random(parentHeight);
 	line(r,i,g,p);
 }
